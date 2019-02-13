5bc9c06a76d4bfda5acda666910afd0bec850a9a
fr3d100 <f.bonnand@gmail.com>
2019-02-13 20:56:17 +0100

    Test in progress

--- spec/controllers/attendances_controller_spec.rb
+++ spec/controllers/attendances_controller_spec.rb
@@ -2,13 +2,6 @@ require 'rails_helper'
 
 RSpec.describe AttendancesController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
-
   describe "GET #new" do
     it "returns http success" do
       get :new
@@ -19,20 +12,6 @@ RSpec.describe AttendancesController, type: :controller do
   describe "GET #create" do
     it "returns http success" do
       get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
       expect(response).to have_http_status(:success)
     end
   end
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe EventsController, type: :controller do
     end
   end
 
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
